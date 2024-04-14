d04496a9b20d8f88af79bdf90968ebfe4c2bbe5a
Yash Pareshbhai Joshi <112645426+Yash1943@users.noreply.github.com>
2024-04-14 11:21:56 +0530

    Update settings.py

--- Sports_Guide/settings.py
+++ Sports_Guide/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = '2021095900023267_it@spcevng.ac.in'
-EMAIL_HOST_PASSWORD = 'Yash@194'
+EMAIL_HOST_USER = 'Your_main_id'
+EMAIL_HOST_PASSWORD = 'password '
 EMAIL_PORT = 587
 
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
